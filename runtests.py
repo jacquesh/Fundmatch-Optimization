a4ebb15d50bd20e67cb05c86ad17eb67c845b75c
Jacques Heunis <jaaksemail@gmail.com>
2016-11-12 16:21:18 +0200

    Normalize the allocCount and fitness runtests output for plotting

--- runtests.py
+++ runtests.py
@@ -30,13 +30,22 @@ if __name__ == "__main__":
     for f in plot_files:
         f.write("\n")
 
+    ds_requirement_count = [-1 for ds in args.data_set]
     for index, data_set in enumerate(args.data_set):
-        if not isfile("./data/%s_requirements.csv" % data_set):
+        require_filepath = "./data/%s_requirements.csv" % data_set
+        if not isfile(require_filepath):
             print("Data set %s unrecognized, ignoring" % data_set)
             del args.data_set[index]
+        else:
+            with open(require_filepath) as req_file:
+                require_text = req_file.read()
+                ds_requirement_count[index] = require_text.count("\n")-1
 
-    for data_set in args.data_set:
-        print("Running tests for dataset %s" % data_set)
+    for req_count, data_set in zip(ds_requirement_count, args.data_set):
+        worstcaseOutput = check_output(["./build/worstcase", data_set]).decode()
+        regexMatch = re.search(r"fitness was (-?\d+\.\d+) from (\d+) allocations", worstcaseOutput)
+        worstFitness = float(regexMatch.group(1))
+        print("Running tests for dataset %s, normalized to %.2f" % (data_set, worstFitness))
         for f in plot_files:
             f.write(data_set)
         for method in args.method:
@@ -66,8 +75,8 @@ if __name__ == "__main__":
                 averageFitness = 0
                 averageAllocations = 0
                 averageRuntime = 0
-            fitness_plot_file.write(" %.2f" % averageFitness)
-            alloc_count_plot_file.write(" %.2f" % averageAllocations)
+            fitness_plot_file.write(" %.2f" % (averageFitness/worstFitness))
+            alloc_count_plot_file.write(" %.2f" % (averageAllocations/req_count))
             runtime_plot_file.write(" %.2f" % averageRuntime)
             print("\tAverage fitness: %.2f" % averageFitness)
             print("") # Get us an empty line
